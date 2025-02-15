161924947314bf1ac9b52f0523172f72a9c760c5
kadirnar <kadir.nar@hotmail.com>
2025-02-15 15:38:02 +0000

    🐛 Update setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import re
 
 import setuptools
 
+NAME = "stylettsplus"
+
 
 def get_requirements(req_path: str):
     with open(req_path, encoding='utf8') as f:
@@ -20,35 +22,34 @@ def get_long_description():
 
 def get_version():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    version_file = os.path.join(current_dir, 'openvoiceplus', '__init__.py')
+    version_file = os.path.join(current_dir, NAME, '__init__.py')
     with open(version_file, encoding='utf-8') as f:
         return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_author():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, 'openvoiceplus', '__init__.py')
+    init_file = os.path.join(current_dir, NAME, '__init__.py')
     with open(init_file, encoding='utf-8') as f:
         return re.search(r'^__author__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_license():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, 'openvoiceplus', '__init__.py')
+    init_file = os.path.join(current_dir, NAME, '__init__.py')
     with open(init_file, encoding='utf-8') as f:
         return re.search(r'^__license__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 setuptools.setup(
-    name='openvoiceplus',
+    name=NAME,
     version=get_version(),
     author=get_author(),
-    author_email='kadir.nar@hotmail.com',
+    author_email='kadirnar.dev@hotmail.com',
     license=get_license(),
-    description="OpenVoicePlus: Advanced python library for OpenVoicePlus",
+    description="StyleTTSPlus: Advanced python library for StyleTTS",
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
-    url='https://github.com/kadirnar/OpenVoicePlus',
+    url='https://github.com/kadirnar/stylettsplus',
     install_requires=INSTALL_REQUIRES,
-    packages=setuptools.find_packages(),
 )
